db2ea55c8a44ece4b7bfa9c075036cbb450bea06
Rajath Shettigar <111166897+rajathshttgr@users.noreply.github.com>
2024-12-10 09:26:59 +0530

    Update Sidebar.jsx (removed tree Data)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { BsChevronDown, BsChevronRight } from "react-icons/bs";
 import { AiOutlineFileAdd } from "react-icons/ai";
 import { FiFolderPlus } from "react-icons/fi";
 import { Tooltip } from "bootstrap";
+import treeData from "";
 
 const Sidebar = () => {
   const projectName = "EVALVATION:";
@@ -25,58 +26,7 @@ const Sidebar = () => {
     });
   }, []);
 
-  const [tree, setTree] = useState([
-    {
-      name: "Documents",
-      type: "folder",
-      isOpen: true,
-      children: [
-        { name: "Document1.jpg", type: "file" },
-        { name: "Document2.jpg", type: "file" },
-        { name: "Document3.jpg", type: "file" },
-      ],
-    },
-    {
-      name: "Desktop",
-      type: "folder",
-      isOpen: true,
-      children: [
-        { name: "Screenshot1.jpg", type: "file" },
-        { name: "videopal.mp4", type: "file" },
-      ],
-    },
-    {
-      name: "Downloads",
-      type: "folder",
-      isOpen: true,
-      children: [
-        {
-          name: "Drivers",
-          type: "folder",
-          isOpen: true,
-          children: [
-            { name: "Printerdriver.dmg", type: "file" },
-            { name: "cameradriver.dmg", type: "file" },
-          ],
-        },
-      ],
-    },
-    {
-      name: "Applications",
-      type: "folder",
-      isOpen: true,
-      children: [
-        { name: "Webstorm.dmg", type: "file" },
-        { name: "Pycharm.dmg", type: "file" },
-        { name: "Webstorm.dmg", type: "file" },
-        { name: "Pycharm.dmg", type: "file" },
-      ],
-    },
-    {
-      name: "chromedriver.dmg",
-      type: "file",
-    },
-  ]);
+  
 
   const toggleFolder = (path) => {
     const updateTree = (nodes, pathIndex = 0) => {
